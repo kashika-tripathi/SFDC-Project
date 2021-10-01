45395f5552a49fb28e426c5ebf722a45c9b633bc
Mukul-Ksolves <mukul.mishra@ksolves.com>
2021-10-01 16:49:26 +0530

    added

--- force-app/main/default/aura/VideoForm/VideoFormController.js
+++ force-app/main/default/aura/VideoForm/VideoFormController.js
@@ -1,12 +1,24 @@
 ({
-    handleClick : function (cmp, event, helper) {
-        var videoFormSubmit = $A.get("e.c:VideoFormSubmit");
-        // var videoFormSubmit=component.getEvent('videoFormSubmit');
+	fireDisplayVideoEvent : function(cmp, event) {
+        var appEvent = $A.get("e.c:displayVideo");
+        if(appEvent.getParam("show")==false){
+            appEvent.setParams({
+            "show" : true });
+        } else if(appEvent.getParam("show")==true){
+            appEvent.setParams({
+            "show" : false });
+        }else {
+            appEvent.setParams({
+            "show" : true });
+        }
+        appEvent.fire();
+    },
+    handleUrlClick : function (cmp, event, helper) {
+        var videoFormSubmit = $A.get("e.c:videoSearchFormSubmit");
+        var enteredUrl=cmp.find("urlInput").get("v.value");
         videoFormSubmit.setParams({
-            "videoTypeId" : "https://www.youtube.com/watch?v=e0EmESimI7Q"
+            "url" : enteredUrl
         })
         videoFormSubmit.fire();
-        
     }
-});
-
+})
