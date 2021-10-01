ac4f01e3a0d7b9326d4f449c916fbe3a2b11c111
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-10-01 15:18:00 +0530

    created the controller for video detail list

--- force-app/main/default/aura/videoDetailsList/videoDetailsListController.js
+++ force-app/main/default/aura/videoDetailsList/videoDetailsListController.js
@@ -1,15 +1,43 @@
+// ({
+//     clickCreateItem: function(component, event, helper) {
+//         var validRecord = component.find('videoform').reduce(function (validSoFar, inputCmp) {
+//             inputCmp.showHelpMessageIfInvalid();
+//             return validSoFar && inputCmp.get('v.validity').valid;
+//         }, true);
+//         if(validRecord){
+//             var newRecordItem = component.get("v.newItem");
+//             var records = component.get("v.items");
+//             var item = JSON.parse(JSON.stringify(newRecordItem));
+//             records.push(item);
+//             component.set("v.items",records);
+//         }
+//     }
+// })
+
 ({
-    clickCreateItem: function(component, event, helper) {
-        var validRecord = component.find('videoform').reduce(function (validSoFar, inputCmp) {
-            inputCmp.showHelpMessageIfInvalid();
-            return validSoFar && inputCmp.get('v.validity').valid;
-        }, true);
-        if(validRecord){
-            var newRecordItem = component.get("v.newItem");
-            var records = component.get("v.items");
-            var item = JSON.parse(JSON.stringify(newRecordItem));
-            records.push(item);
-            component.set("v.items",records);
-        }
-    }
+    handleCreateVideo: function(component, event) {
+        var saveVideoAction = component.get("c.createVideo");
+            saveVideoAction.setParams({
+                "video": component.get("v.newVideoRecord")
+            });
+
+            console.log(video);
+        
+        // Configure the response handler for the action
+        saveVideoAction.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === "SUCCESS") {
+                component.set("v.message", "Video created successfully");
+            }
+            else if (state === "ERROR") {
+                console.log('Problem saving Video, response state: ' + state);
+            }
+            else {
+                console.log('Unknown problem, response state: ' + state);
+            }
+        });
+
+        // Send the request to create the new Video
+        $A.enqueueAction(saveVideoAction);
+    },
 })
