eb6293033b076d121d0b8280ecb41b99e9c749cf
kashika <kashika.tripathi_cs18@gla.ac.in>
2021-10-01 16:21:09 +0530

    video detail list completed

--- force-app/main/default/aura/videoDetailsList/videoDetailsListController.js
+++ force-app/main/default/aura/videoDetailsList/videoDetailsListController.js
@@ -1,18 +1,4 @@
-// ({
-//     clickCreateItem: function(component, event, helper) {
-//         var validRecord = component.find('videoform').reduce(function (validSoFar, inputCmp) {
-//             inputCmp.showHelpMessageIfInvalid();
-//             return validSoFar && inputCmp.get('v.validity').valid;
-//         }, true);
-//         if(validRecord){
-//             var newRecordItem = component.get("v.newItem");
-//             var records = component.get("v.items");
-//             var item = JSON.parse(JSON.stringify(newRecordItem));
-//             records.push(item);
-//             component.set("v.items",records);
-//         }
-//     }
-// })
+
 
 ({
     handleCreateVideo: function(component, event) {
@@ -30,6 +16,7 @@
                 component.set("v.message", "Video created successfully");
             }
             else if (state === "ERROR") {
+                console.log(response.getError());
                 console.log('Problem saving Video, response state: ' + state);
             }
             else {
