47c5dec971dc8473d0e4a8329c73460062a251f4
Mukul-Ksolves <mukul.mishra@ksolves.com>
2021-10-11 10:36:32 +0530

    added picklist for categories

--- force-app/main/default/aura/VideoTiles/VideoTilesController.js
+++ force-app/main/default/aura/VideoTiles/VideoTilesController.js
@@ -1,11 +1,14 @@
 ({
     initialiseVideos : function(component, event, helper) {
+        var selectedCategory = component.find("onjId").get("v.value");
         var action = component.get("c.getVideos"); 
+        action.setParams({"category": selectedCategory});
          action.setCallback(this, function(response) {
              var state = response.getState();
              if (state === "SUCCESS") {           
                  var allVideos = response.getReturnValue();
                  component.set("v.videos", allVideos);
+                 
              }                    
              else if (state === "ERROR") {
                  var errors = response.getError();
